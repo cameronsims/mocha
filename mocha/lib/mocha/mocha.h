f7935b19f9eae648c3c0f398aec92771482dc9e5
cameronsims <59544722+cameronsims@users.noreply.github.com>
2020-08-06 17:20:43 +0800

    Update mocha.h

--- mocha/lib/mocha/mocha.h
+++ mocha/lib/mocha/mocha.h
@@ -1,24 +1,79 @@
 /* mocha.h
- * * mocha::endl
+ * * mocha::size_t
+ * * mocha::time_t
  * * mocha::size()
+ * * mocha::exception()
  */
 #pragma once
+#define MOCHA true
 namespace mocha {
-    static constexpr char endl = '\n';
+    /* mocha::size_t */
+    typedef unsigned int size_t;
+    /* mocha::time_t */
+    typedef __int64      time_t;
 
     /* mocha::size()
-     * * NOTE: ARRAY MUST BE DEFINED IN STACK TO WORK PROPERLY
-     * * TEMPLATE: { "T": "Typename to use" }
-     * * "arr": "Get size of"
+     * * NOTE: USE "MOCHA_SIZE()" FOR STACK DEFINED ARRAY
+     * * "_arr": "Get size of"
      */
     #define MOCHA_SIZE(_arr) sizeof(_arr) / sizeof(*_arr)
-    template<typename T>
-    size_t size(const T arr[]) {
-        return MOCHA_SIZE(arr);
+    size_t size (const char* _arr) {
+        size_t l{};
+        while (_arr[l] != '\0') l++;
+        return l;
+    }   
+    size_t bytes(const char* _arr) {
+        return size(_arr) * sizeof(char);
     }
-    size_t size(const char* arr) {
-        int l{};
-        while (arr[l] != '\0') l++;
+
+    #ifdef _ARRAY_
+    template<typename T, const int l>
+    size_t size(const std::array<T, l>& a) {
         return l;
     }
+    template<typename T, const int l>
+    size_t bytes(const std::array<T, l>& a) {
+        return l * sizeof(T);
+    }
+    #ifdef _VECTOR_
+    template<typename T>
+    size_t size(const std::vector<T>& v) {
+        return v.size();
+    }
+    template<typename T>
+    size_t bytes(const std::vector<T>& v) {
+        return v.size() * sizeof(T);
+    }
+    #endif
+    #endif
+
+    /* mocha::exception() */
+    class exception {
+      protected:
+        static size_t iterations;
+        const char* what;
+      public:
+        exception(                )           noexcept { iterations++; };
+        exception(const exception&)           noexcept { iterations++; };
+        explicit exception(const char* _what) noexcept 
+            : what(_what)
+        {
+            #ifdef MOCHA_DEBUG
+            printf("[mocha]: \"Exception created!\"\n");
+            #endif
+            iterations++;
+        }
+        ~exception() {
+            #ifdef MOCHA_DEBUG
+            printf("[mocha]: \"Exception removed!\"\n");
+            #endif
+        }
+        
+        void operator= (const exception& _r) { what = _r.what; }
+
+        const char*   reason() { return what; }
+        static size_t amount() { return iterations; }
+    };
 }
+
+mocha::size_t mocha::exception::iterations = 0;
